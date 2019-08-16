e67b7e28298c16016c1fcb3f6cc5372f1f41720e
liuwenjian <liuwenjian091@pingan.com.cn>
2019-08-16 15:24:30 +0800

    auto commit

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8acdd2ffc593dadebc3ec5f0b7e09a8c.js"
+  "./precache-manifest.c903304635cd922557b5d4aabd8851ab.js"
 );
 
 workbox.clientsClaim();
